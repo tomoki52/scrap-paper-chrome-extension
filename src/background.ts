1ca063d5d968bfd96cb2d930b43bd78134d98439
tomoki52 <konishi.tomoki.ap@alumni.tsukuba.ac.jp>
2023-01-02 19:12:25 +0900

    add main feature

--- src/background.ts
+++ src/background.ts
@@ -1,6 +1,31 @@
-function polling() {
-  // console.log("polling");
-  setTimeout(polling, 1000 * 30);
-}
+chrome.action.onClicked.addListener((tab: chrome.tabs.Tab) => {
+  if (tab.id == undefined) {
+    console.log("undefined.");
+    return;
+  }
+  chrome.tabs.sendMessage(tab.id, "register", (response) => {
+    console.log(response);
+  });
+});
 
-polling();
+const updateContextMenus = async () => {
+  await chrome.contextMenus.removeAll();
+  const type = "normal";
+  chrome.contextMenus.create({
+    id: "options",
+    type,
+    title: "options",
+    contexts: ["all"],
+  });
+};
+
+chrome.runtime.onInstalled.addListener(updateContextMenus);
+chrome.runtime.onStartup.addListener(updateContextMenus);
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  switch (info.menuItemId) {
+    case "options":
+      chrome.tabs.create({ url: "options.html" });
+      break;
+  }
+});
--- src/content_script.tsx
+++ src/content_script.tsx
@@ -1,9 +1,68 @@
-chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-  if (msg.color) {
-    console.log("Receive color = " + msg.color);
-    document.body.style.backgroundColor = msg.color;
-    sendResponse("Change color to " + msg.color);
-  } else {
-    sendResponse("Color message is none.");
-  }
+const proj: string = "tomoki52";
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  console.log("recieved");
+  console.log(request);
+  registerScrapBox(proj);
+  sendResponse("from content script");
 });
+
+function registerScrapBox(proj: string) {
+  const url: string = location.href;
+
+  const addLink = (text: string) => {
+    return "[" + text + "]";
+  };
+
+  const title_collection = document.getElementsByClassName(
+    "citation__title"
+  ) as HTMLCollectionOf<HTMLElement>;
+
+  const title = title_collection[0].innerText;
+
+  const author_collection = document.getElementsByClassName(
+    "author-name"
+  ) as HTMLCollectionOf<HTMLElement>;
+
+  const authors = Array.from(author_collection).map((e) => {
+    return addLink(e.title);
+  });
+
+  const epub_section_title_collection = document.getElementsByClassName(
+    "epub-section__title"
+  ) as HTMLCollectionOf<HTMLElement>;
+
+  const conference_with_year =
+    epub_section_title_collection[0].innerText.split(":")[0];
+
+  const abstract_collection = document.getElementsByClassName(
+    "abstractSection abstractInFull"
+  ) as HTMLCollectionOf<HTMLElement>;
+
+  const abstract = abstract_collection[0].children[0].textContent;
+
+  const conference: string = conference_with_year.split(" '")[0];
+
+  const lines: string =
+    url +
+    "\n" +
+    addLink(conference) +
+    " " +
+    addLink(conference_with_year) +
+    "\n" +
+    authors.join(", ") +
+    "\n\n" +
+    ">" +
+    abstract +
+    "\n\n" +
+    "#survey";
+
+  var body = encodeURIComponent(lines);
+  window.open(
+    "https://scrapbox.io/" +
+      proj +
+      "/" +
+      encodeURIComponent(title.trim()) +
+      "?body=" +
+      body
+  );
+}
